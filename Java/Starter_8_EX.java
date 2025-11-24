fe65257bad38feb5e141fb9b8200611f8e4ffe99
Weiss_rN <darma20060311@gmail.com>
2025-11-24 11:35:34 +0000

    2035

---
+++ Java/Starter_8_EX.java
@@ -0,0 +1,5 @@
+public class Starter_8_EX {
+    public static void main(String[] args) {
+        String 
+    }
+}
---
+++ Java/Starter_9.java
@@ -0,0 +1,9 @@
+public class Starter_9 {
+    // Java program to demonstrate the use of final keyword with arrays.
+    public static void main(String[] args) {
+        final int[] numbers = {10, 20, 30, 40, 50};
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
